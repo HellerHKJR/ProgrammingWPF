367ef46127c907de27309a1ae8a4690c44ffe4a1
jryou <jryu@hellerindustries.co.kr>
2018-08-14 16:56:16 +0900

    Other01 Custom Command Creating command using ICommand interface

---
+++ Other01_Command/CommandList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Other01_Command
+{
+    public class AddCommand : ICommand
+    {
+        private string desc = "Add";
+        public string DESC { get => desc; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine("Add 명령 실행");
+        }
+
+        public AddCommand()
+        {
+            Console.WriteLine("AddCommand Constructor");
+        }
+    }
+}
