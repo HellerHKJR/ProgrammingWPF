aed9cd16c51049d2130596423201632fe0862156
jryou <jryu@hellerindustries.co.kr>
2018-08-14 09:15:13 +0900

    Initial P028 Image and Gif on Button

---
+++ P028_MyFirstWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P028_MyFirstWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void gifGirl_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            gifGirl.Position = new TimeSpan(0, 0, 1);
+            gifGirl.Play();
+        }
+    }
+}
