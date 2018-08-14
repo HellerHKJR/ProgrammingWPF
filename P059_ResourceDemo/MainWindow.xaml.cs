857dae78d12e04be11e10ba3fb182d315dbca36b
jryou <jryu@hellerindustries.co.kr>
2018-08-14 11:11:56 +0900

    P059 Resource Create Resoruce that has Key name. Add Resource in XAML or FindResource in Code. DataContext could be specified in manner of StaticResource.

---
+++ P059_ResourceDemo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P059_ResourceDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Nicknames names;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            names = (Nicknames)FindResource("names");
+            // 이렇게 할 경우 DataContext를 코드 상에서 지정할 필요가 없다.
+
+            names.Add(new Nickname("XXX", "1st"));
+            names.Add(new Nickname("YYY", "2nd"));
+            names.Add(new Nickname("ZZZ", "3rd"));
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            names.Add(new Nickname(txtName.Text, txtNick.Text));
+        }
+    }
+}
---
+++ P059_ResourceDemo/Nickname.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace P059_ResourceDemo
+{
+    public class Nickname : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange(string pName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(pName));
+        }
+
+        string name;
+        public string Name { get => name; set { name = value; OnPropertyChange("Name"); } }
+        string nick;
+        public string Nick { get => nick; set { nick = value; OnPropertyChange("Nick"); } }
+
+        public Nickname() : this("Name", "Nick") { }
+
+        public Nickname(string name, string nick)
+        {
+            this.name = name;
+            this.nick = nick;
+        }
+    }
+
+    public class Nicknames : ObservableCollection<Nickname> { }
+}
